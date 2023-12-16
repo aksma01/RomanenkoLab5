93e8450ef78d6134f0a8ee16eaf0abd0e11b9557
aksma01 <146925177+aksma01@users.noreply.github.com>
2023-12-16 17:53:45 +0200

    Add files via upload

---
+++ Romanenko_lab5/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Romanenko_lab5
+{
+    public abstract class Vehicle
+    {
+        public int Speed { get; set; }
+        public int Capacity { get; set; }
+
+        public abstract void Move();
+    }
+
+    public class Human : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Walking...");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Driving a car...");
+        }
+    }
+
+    public class Bus : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Riding a bus...");
+        }
+    }
+
+    public class Train : Vehicle
+    {
+        public override void Move()
+        {
+            Console.WriteLine("Taking a train...");
+        }
+    }
+
+    public class Route
+    {
+        public string StartPoint { get; set; }
+        public string EndPoint { get; set; }
+
+        public Route(string startPoint, string endPoint)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+    }
+
+    public class TransportNetwork
+    {
+        private List<Vehicle> vehicles;
+
+        public TransportNetwork()
+        {
+            vehicles = new List<Vehicle>();
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+
+        public void MoveAllVehicles()
+        {
+            foreach (var vehicle in vehicles)
+            {
+                vehicle.Move();
+            }
+        }
+
+        public void CalculateOptimalRoute(Route route, Vehicle vehicle)
+        {
+            Console.WriteLine($"Calculating optimal route from {route.StartPoint} to {route.EndPoint} for {vehicle.GetType().Name}");
+        }
+
+        public void BoardPassengers(Vehicle vehicle)
+        {
+            Console.WriteLine($"Boarding passengers on {vehicle.GetType().Name}");
+        }
+
+        public void DisembarkPassengers(Vehicle vehicle)
+        {
+            Console.WriteLine($"Disembarking passengers from {vehicle.GetType().Name}");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            TransportNetwork transportNetwork = new TransportNetwork();
+
+            Car car = new Car { Speed = 60, Capacity = 5 };
+            Bus bus = new Bus { Speed = 40, Capacity = 20 };
+            Train train = new Train { Speed = 80, Capacity = 100 };
+
+            transportNetwork.AddVehicle(car);
+            transportNetwork.AddVehicle(bus);
+            transportNetwork.AddVehicle(train);
+
+            transportNetwork.MoveAllVehicles();
+
+            Route route = new Route("City A", "City B");
+
+            transportNetwork.CalculateOptimalRoute(route, car);
+            transportNetwork.BoardPassengers(car);
+            transportNetwork.DisembarkPassengers(car);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    public class MathOperations
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int[] Add(int[] array1, int[] array2)
+        {
+            if (array1.Length != array2.Length)
+            {
+                throw new ArgumentException("Arrays must have the same length");
+            }
+
+            int[] result = new int[array1.Length];
+            for (int i = 0; i < array1.Length; i++)
+            {
+                result[i] = array1[i] + array2[i];
+            }
+
+            return result;
+        }
+
+        public static int[,] Add(int[,] matrix1, int[,] matrix2)
+        {
+            if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+            {
+                throw new ArgumentException("Matrices must have the same dimensions");
+            }
+
+            int[,] result = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int sum = MathOperations.Add(5, 3);
+            Console.WriteLine($"Sum of numbers: {sum}");
+
+            int[] array1 = { 1, 2, 3 };
+            int[] array2 = { 4, 5, 6 };
+            int[] arraySum = MathOperations.Add(array1, array2);
+            Console.WriteLine($"Sum of arrays: [{string.Join(", ", arraySum)}]");
+
+            int[,] matrix1 = { { 1, 2 }, { 3, 4 } };
+            int[,] matrix2 = { { 5, 6 }, { 7, 8 } };
+            int[,] matrixSum = MathOperations.Add(matrix1, matrix2);
+            Console.WriteLine("Sum of matrices:");
+            for (int i = 0; i < matrixSum.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixSum.GetLength(1); j++)
+                {
+                    Console.Write($"{matrixSum[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
